2e69b547ee921108d7a8523d0752a409b521f1f0
Alexander Aranda <Alexandermarkets1@gmail.com>
2021-09-11 20:29:32 -0700

    server: models: user.js setup

--- server/models/User.js
+++ server/models/User.js
@@ -30,3 +30,28 @@ const userSchema = new Schema(
       virtuals: true,
     },
   }
+);
+
+// hash user password
+userSchema.pre('save', async function (next) {
+    if (this.isNew || this.isModified('password')) {
+      const saltRounds = 10;
+      this.password = await bcrypt.hash(this.password, saltRounds);
+    }
+  
+    next();
+  });
+  
+  // custom method to compare and validate password for logging in
+  userSchema.methods.isCorrectPassword = async function (password) {
+    return bcrypt.compare(password, this.password);
+  };
+  
+  // when we query a user, we'll also get another field called `bookCount` with the number of saved books we have
+  userSchema.virtual('bookCount').get(function () {
+    return this.savedBooks.length;
+  });
+  
+  const User = model('User', userSchema);
+  
+  module.exports = User;
